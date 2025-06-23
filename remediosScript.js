78c416dbeea96766951bc46697769fc14d87f7b9
dearerknoot <thiagocarlesso08@gmail.com>
2025-06-23 19:25:57 -0300

    coommit

--- remediosScript.js
+++ remediosScript.js
@@ -1,5 +1,5 @@
 const voltar = document.querySelector(".voltarIMG");
 
 voltar.addEventListener("click",()=>{
-    window.location.href = "paginaPrincipal.html";
+    window.location.href = "index.html";
 });
