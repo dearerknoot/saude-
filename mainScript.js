bd35645d0eccd556889ed3048d1e3db5c05bdb8b
dearerknoot <thiagocarlesso08@gmail.com>
2025-07-22 20:57:39 -0300

    mvp finished

--- mainScript.js
+++ mainScript.js
@@ -2,6 +2,8 @@ const remediosBtn = document.getElementById('remedios');
 const agendaBtn = document.getElementById('consultas');
 const tarefasBtn = document.getElementById('tarefas');
 const infoBtn = document.getElementById('info');
+const saudeBtn = document.getElementById('saude');
+const emergencia = document.getElementById('contato');
 agendaBtn.addEventListener('click',()=>{
     window.location.href = 'calendario.html';
 });
@@ -14,3 +16,9 @@ tarefasBtn.addEventListener('click',()=>{
 infoBtn.addEventListener('click',()=>{
     window.location.href = 'info.html';
 })
+saudeBtn.addEventListener('click',()=>{
+    window.location.href = 'saude.html';
+});
+emergencia.addEventListener('click',()=>{
+    window.location.href = 'emergencia.html';
+});
